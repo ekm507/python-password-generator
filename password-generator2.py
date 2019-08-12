cb4b084c4329a782bc6e469d97e5bb7a579d5141
Erfan Kheyrollahi <ekm507@gmail.com>
2019-08-12 22:55:48 +0430

    Create password-generator2.py

---
+++ password-generator2.py
@@ -0,0 +1,42 @@
+
+import secrets
+from secrets import choice
+from io import open
+
+verbsFile = open('verbs.txt', 'r', encoding='utf-8')
+thingsFile = open('things.txt', 'r', encoding='utf-8')
+
+verbsSet = set()
+
+for verb in verbsFile:
+    verbsSet.add(verb.rstrip('\n') + 's')
+
+verbsList = list(verbsSet)
+
+thingsSet = set()
+
+for thing in thingsFile:
+    thingsSet.add(thing.rstrip('\n'))
+
+thingsList = list(thingsSet)
+
+
+minsize = 20
+maxsize = 25
+
+passwordSize = maxsize + 1
+
+while(passwordSize > maxsize or passwordSize < minsize):
+    password = choice(thingsList)
+    passwordSize = len(password)
+    word = choice(verbsList)
+    password += ' ' + word
+    word = choice(thingsList)
+    password += ' ' + word
+    passwordSize = len(password)
+
+print(password, passwordSize)
+
+
+thingsFile.close()
+verbsFile.close()
